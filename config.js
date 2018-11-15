6867bed45c8585b62ddc754ee2a3faeb519dbe98
juneandgreen <june_01@qq.com>
2018-11-15 15:46:01 +0800

    update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     index: 'https://weixin.qq.com/index?a=1&b=2#hash',
   },
   resFilter(src, pageKey) {
-    return pageKey === 'index' && src === './index.png' ? 'http://juneandgreen.github.io/test/h5-to-miniprogram-demo/demo1/logo.png' : src
+    return pageKey === 'index' && src === './index.png' ? 'https://raw.githubusercontent.com/wechat-miniprogram/h5-to-miniprogram-demo/master/h5/index.png' : src
   },
   rem: 58.59375,
 }
