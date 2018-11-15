d2a7e977c6df47aeb89be6fc4e1ce8f2ef18621a
junexie <junexie@tencent.com>
2018-11-15 16:06:24 +0800

    update some

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  index: 'h5',
-  urlMap: {
-    index: 'https://weixin.qq.com/index?a=1&b=2#hash',
+  index: 'h5', // 首页
+  urlMap: { // 每个页面对应的初始 url，用于 window.location 和 window.history 对象
+    h5: 'https://weixin.qq.com/index?a=1&b=2#hash',
   },
   resFilter(src, pageKey) {
+    // 资源过滤，用于替换 h5 中使用到的资源路径
     return pageKey === 'h5' && src === './index.png' ? 'https://raw.githubusercontent.com/wechat-miniprogram/h5-to-miniprogram-demo/master/h5/index.png' : src
   },
-  rem: 58.59375,
 }
