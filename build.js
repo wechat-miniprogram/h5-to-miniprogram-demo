a9f6f2789f6282206cf9448d5a8e4e87a6adcce8
junexie <junexie@tencent.com>
2018-11-15 16:30:33 +0800

    update some

--- build.js
+++ build.js
@@ -2,11 +2,11 @@ const path = require('path')
 const toMiniprogram = require('h5-to-miniprogram')
 
 toMiniprogram({
-  entry: {
+  entry: { // 入口 h5 页面路径
     h5: path.join(__dirname, './h5/index.html'),
   },
-  output: path.join(__dirname, './miniprogram'),
-  config: path.join(__dirname, './config.js'),
+  output: path.join(__dirname, './miniprogram'), // 输出目录
+  config: path.join(__dirname, './config.js'), // 配置文件路径
 }).then(res => {
   console.log('done')
 }).catch(err => {
